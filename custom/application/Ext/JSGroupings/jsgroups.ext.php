25909ca27bed2def8acb30dfd7f3bf8b81ef54fa
Nikola <nikola.jovanovic@qposoft.com>
2019-09-23 02:47:28 +0200

    #5 JsGroupings two dummy js files

---
+++ custom/application/Ext/JSGroupings/jsgroups.ext.php
@@ -0,0 +1,12 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+
+//creates the file cache/include/javascript/newGrouping.js
+$js_groupings[] = $newGrouping = array(
+    'custom/js/file1.js' => 'include/javascript/newGrouping.js',
+    'custom/js/file2.js' => 'include/javascript/newGrouping.js',
+);
+
+?>
